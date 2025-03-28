59daecad177c955e4af46275b4d62f7e2b3041d3
juandnb01 <juanmothy182@gmail.com>
2025-03-28 15:19:35 -0500

    firts commit

---
+++ src/main/java/com/urbango/urbango_backend/UrbanGo/Backend/UrbanGoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.urbango.urbango_backend.UrbanGo.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanGoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanGoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urbango/urbango_backend/UrbanGo/Backend/UrbanGoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbango.urbango_backend.UrbanGo.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanGoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
